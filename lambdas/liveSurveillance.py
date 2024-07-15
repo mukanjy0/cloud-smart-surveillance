8ebc699738e62e6a9953d2428d9972526e83dcc5
mukanjy0 <mukanjyo29@protonmail.com>
2024-07-15 02:06:29 -0500

    liveSurveillance

--- lambdas/liveSurveillance.py
+++ lambdas/liveSurveillance.py
@@ -13,40 +13,52 @@ sns_topic_arn = os.environ['SNS_TOPIC_ARN']
 def lambda_handler(event, context):
     print(event)
 
-    body = event['body']
-    bucket_name = body['bucket_name']
-    image_key = body['image_key']
-    
-    try:
-        search_response = rekognition.search_faces_by_image(
-            CollectionId=people_collection,
-            Image={
-                'S3Object': {
-                    'Bucket': bucket_name,
-                    'Name': image_key
-                }
-            },
-            FaceMatchThreshold=80,
-            QualityFilter='AUTO'
-        )
+    face_matches = []
+    for record in event['Records']:
+      sqs_message_body = record['body']
+      sqs_message_json = json.loads(sqs_message_body)
+      sns_message = sqs_message_json['Message']
+      
+      print(f"Received SNS message: {sns_message}")
+      
+      try:
+        message_data = json.loads(sns_message)
+        
+        bucket_name = message_data['bucket_name']
+        image_key = message_data['image_key']
 
-        # if any of the matches is an offender
-        sns.publish(
-            TopicArn=sns_topic_arn,
-            Message=json.dumps({
-                'FaceMatches': search_response['FaceMatches']
-            }),
-        )
+        try:
+            search_response = rekognition.search_faces_by_image(
+                CollectionId=people_collection,
+                Image={
+                    'S3Object': {
+                        'Bucket': bucket_name,
+                        'Name': image_key
+                    }
+                },
+                FaceMatchThreshold=80,
+                QualityFilter='AUTO'
+            )
 
-        return {
-            'statusCode': 200,
-            'body': json.dumps({
-                'faceDetails': search_response['FaceMatches']
-            })
-        }
+            print(search_response)
 
-    except Exception as e:
-        return {
-            'statusCode': 500,
-            'body': json.dumps({'error': str(e)})
-        }
+            face_matches.extend(search_response['FaceMatches'])
+
+            # if any of the matches is an offender
+            sns.publish(
+                TopicArn=sns_topic_arn
+            )
+
+        except Exception as e:
+            print(json.dumps({'error': str(e)}))
+
+      except json.JSONDecodeError:
+          print("Received message is not in JSON format")
+      
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps({
+            'faceDetails': face_matches
+        })
+    }
