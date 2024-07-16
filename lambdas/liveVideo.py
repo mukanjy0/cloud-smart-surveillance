bb2132d9b2c9339afdd53cd55c1471d4ecc435e8
mukanjy0 <mukanjyo29@protonmail.com>
2024-07-15 19:57:25 -0500

    replaced pandas with csv module

--- lambdas/liveVideo.py
+++ lambdas/liveVideo.py
@@ -1,8 +1,7 @@
 import os
+import csv
 import boto3
 import json
-import pandas as pd
-from datetime import datetime
 
 s3 = boto3.client('s3')
 sns = boto3.client('sns')
@@ -12,17 +11,30 @@ lambda_client = boto3.client('lambda')
 bucket_name = os.environ['BUCKET_NAME']
 sns_topic_arn = os.environ['SNS_TOPIC_ARN']
 
+filename = 'frame.csv'
+
 def create_frame_csv(items):
-  with open('frame.csv', 'w') as f:
-      f.write('tenant_id,n_frames,cur_frame') 
+  with open(filename, 'w') as f:
+      f.write('tenant_id,n_frames,cur_frame\n') 
 
   for tenant_id in items:
-    with open('fps.csv', 'a') as f:
+    with open(filename, 'a') as f:
         f.write(f'{tenant_id},240,0\n')
+
+def update_frame_csv(data):
+  with open(filename, 'w') as f:
+      f.write('tenant_id,n_frames,cur_frame\n') 
+
+  with open(filename, 'a') as f:
+    for row in data:
+      tenant_id = row['tenant_id']
+      n_frames = row['n_frames']
+      cur_frame = row['cur_frame']
+      f.write(f'{tenant_id},{n_frames},{cur_frame}\n')
 
 def simulate_stream(tenant_id, stream_metadata):
     try:
-      cur_frame = stream_metadata[stream_metadata['tenant_id'] == tenant_id]['cur_frame']
+      cur_frame = stream_metadata['cur_frame']
       image_key = f'{tenant_id}/{cur_frame}.jpg'
 
       sns.publish(
@@ -33,9 +45,7 @@ def simulate_stream(tenant_id, stream_metadata):
           }),
       )
 
-      n_frames = stream_metadata[stream_metadata['tenant_id'] == tenant_id]['n_frames']
-      stream_metadata[stream_metadata['tenant_id'] == tenant_id]['cur_frame'] += 1
-      stream_metadata[stream_metadata['tenant_id'] == tenant_id]['cur_frame'] %= n_frames
+      stream_metadata['cur_frame'] = (stream_metadata['cur_frame'] + 1) % stream_metadata['n_frames']
 
     except:
       raise Exception
@@ -52,13 +62,22 @@ def lambda_handler(event, context):
 
     items = response['tenantIds']
 
-    if not os.path.exists('frame.csv'):
+    if not os.path.exists(filename):
       create_frame_csv(items)
 
-    stream_metadata = pd.read_csv('frame.csv')
+    csv_data = csv.DictReader(filename)
+    data = []
+    for row in csv_data:
+      row['n_frames'] = int(row['n_frames'])
+      row['cur_frame'] = int(row['cur_frame'])
+      data.append(row)     
 
     for tenant_id in items:
       try:
+        for row in data:
+          if row['tenant_id'] == tenant_id:
+             stream_metadata = row
+             break
         simulate_stream(tenant_id, stream_metadata)
       except Exception as e:
          print(e)
@@ -67,4 +86,4 @@ def lambda_handler(event, context):
             'body': f'Error in stream simulation'
          }
 
-    stream_metadata.to_csv('frame.csv', index=False)
+    update_frame_csv(data)
