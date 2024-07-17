f2f6c4c03b9098151dcab518e3d48a34394a12e1
mukanjy0 <mukanjyo29@protonmail.com>
2024-07-16 22:06:16 -0500

    fix index faces

--- lambdas/indexFacesCollection.py
+++ lambdas/indexFacesCollection.py
@@ -12,6 +12,7 @@ def lambda_handler(event, context):
 
     response =  s3.list_objects(Bucket=bucket_name, Prefix=prefix)
 
+    faces = dict()
     for image in response['Contents']:
         image_key = image['Key']
 
@@ -24,22 +25,25 @@ def lambda_handler(event, context):
                         'Name': image_key
                     }
                 },
-                ExternalImageId=os.path.basename(image_key),  # Use the file name as the external image ID
+                ExternalImageId=os.path.basename(image_key).split('.')[0],  # Use dni
                 MaxFaces=1,
                 DetectionAttributes=['ALL']
             )
             
-            faces = dict()
             for face_record in index_response['FaceRecords']:
                 face = face_record['Face']
                 faces[face['ExternalImageId']] = {
                     'faceID': face['FaceId'],
                     'Confidence': face['Confidence']
                 }
-            return {
-                'statusCode': 200,
-                'indexedFaces': json.dumps(faces)
-                }
                 
         except Exception as e:
-            print(f'Error indexing faces in {image_key}: {str(e)}')
+            return {
+                'statusCode': 500,
+                'error': f'Error indexing faces in {image_key}: {str(e)}'
+                }
+    
+    return {
+        'statusCode': 200,
+        'body': json.dumps(faces)
+    }
