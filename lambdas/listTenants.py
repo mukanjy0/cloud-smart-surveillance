62091370f36f16734e4f0b43b5ce1c3ae1c5fad3
mukanjy0 <mukanjyo29@protonmail.com>
2024-07-16 20:38:45 -0500

    fix2

--- lambdas/listTenants.py
+++ lambdas/listTenants.py
@@ -4,7 +4,7 @@ import boto3
 dynamodb = boto3.resource('dynamodb')
 security_staff_table_name = os.environ['SECURITY_STAFF_TABLE']
 
-def lambda_handler():
+def lambda_handler(event, context):
     security_staff_table = dynamodb.Table(security_staff_table_name)
 
     query_result = security_staff_table.query(
